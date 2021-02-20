765823bc04da53810337ba152782924237cd9174
NorthropBtwo <elia.fankhauser@bluewin.ch>
2021-02-20 02:13:36 +0100

    added string events

---
+++ Assets/ScriptableObjects/Console/DbgConsole.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DbgConsole : MonoBehaviour
+{
+    public void DebugLog(object msg)
+    {
+        Debug.Log(msg);
+    }
+}
