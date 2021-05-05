902ac4a34b0f1cdf92ab6477b48b9965a323c240
Northrop <elia.fankhauer@bluewin.ch>
2021-05-05 21:20:14 +0200

    added level 3

---
+++ Assets/NorLib/Movement/RotateGo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGo : MonoBehaviour
+{
+
+    public Vector3 RotateDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotateDirection * Time.deltaTime);
+    }
+}
